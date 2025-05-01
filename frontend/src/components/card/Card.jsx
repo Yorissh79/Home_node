47a4bc650fe52a87b1926b739ee8a975871be3ac
Yorissh79 <yusifbsh-azmp203@code.edu.az>
2025-05-01 13:37:27 +0400

    Update

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -1,32 +1,67 @@
 import React from "react";
 import style from "./Card.module.scss";
-import { useDispatch } from "react-redux";
-import { deleteBasketThunk, postBasketThunk } from "../../redux/reducers/basketSlice";
-import { deleteWishThunk, postWishThunk } from "../../redux/reducers/wishSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteBasketThunk, getBasketThunk, postBasketThunk } from "../../redux/reducers/basketSlice";
+import { deleteWishThunk, getWishThunk, postWishThunk } from "../../redux/reducers/wishSlice";
 
 const Card = ({ item, who }) => {
   const dispatch = useDispatch();
 
-  const addToBasket = (object) => {
-    const data = {
-      name: object.name,
-      price: object.price,
-      author: object.author,
-      image: object.image,
-      count: "0",
-    };
+  const value_wish = useSelector(state => state.wish.data)
+  const value_basket = useSelector(state => state.basket.data)
 
-    dispatch(postBasketThunk(data));
+  const addToBasket = (object) => {
+    dispatch(getBasketThunk())
+    if (value_basket){
+      const exist = value_basket.find((item) => item.name === object.name)
+
+      if (!exist) {
+        const data = {
+          name: object.name,
+          price: object.price,
+          author: object.author,
+          image: object.image,
+          count: "0"
+        };
+
+        dispatch(postBasketThunk(data));
+        console.log("elave eledi")
+      } 
+      
+      else {
+        const data = {
+          name: object.name,
+          price: object.price,
+          author: object.author,
+          image: object.image,
+          count: String(Number(object.count) + 1)
+        };
+
+        dispatch(postBasketThunk(data));
+        console.log("var")
+      }
+
+
+    }
   };
 
   const addToWishlist = (object) => {
-    const data = {
-      name: object.name,
-      price: object.price,
-      author: object.author,
-      image: object.image
-    };
-    dispatch(postWishThunk(data));
+    dispatch(getWishThunk())
+
+    if (value_wish){
+      const exist = value_wish.find((item) => item.name === object.name)
+
+      if (!exist) {
+        const data = {
+          name: object.name,
+          price: object.price,
+          author: object.author,
+          image: object.image
+        };
+        dispatch(postWishThunk(data));
+      }
+    }
+
   };
 
   const deleteItem = (kim, id) => {
